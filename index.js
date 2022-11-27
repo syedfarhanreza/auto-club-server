4c3c747ab6fc57c5a889014967833d31bf49f8dc
syedfarhanreza <syedfarhanreza.pub16@gmail.com>
2022-11-27 17:57:19 +0600

    cars collection and mongoDB setup

--- index.js
+++ index.js
@@ -1,12 +1,51 @@
 const express = require('express');
 const cors = require('cors');
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors())
 app.use(express.json())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.0neovd0.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const audiCollection = client.db('autoClub').collection('audiCars');
+        const hondaCollection = client.db('autoClub').collection('hondaCars');
+        const benzCollection = client.db('autoClub').collection('benzCars');
+
+        app.get('/audis', async (req, res) => {
+            const query = {};
+            const result = await audiCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/hondas', async (req, res) => {
+            const query = {};
+            const result = await hondaCollection.find(query).toArray();
+            res.send(result);
+        });
+
+        app.get('/mercedess', async (req, res) => {
+            const query = {};
+            const result = await benzCollection.find(query).toArray();
+            res.send(result);
+        });
+    }
+    finally{
+
+    }
+}
+run().catch(console.log())
+
 
 app.get('/', async(req, res) => {
     res.send('AutoClub server is running')
